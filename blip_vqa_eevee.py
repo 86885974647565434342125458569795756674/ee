64f14220b8b692b79d7195df88ac414c3cd91284
c <c>
2024-07-01 21:49:13 +0800

    cache bug

--- blip_vqa_eevee.py
+++ blip_vqa_eevee.py
@@ -9,34 +9,75 @@ import torch.multiprocessing as multiprocessing
 import sys
 import os
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from collections import OrderedDict
 
 root_path="/dynamic_batch/ee/"
 dataset_dir = root_path+"datasets/vqa/"
 
 sys.path.append(root_path)
-from system.blip.vqa.blip_vqa_process import blip_vqa_process
-from system.utils import change_batch_size
-from system.cache import cache_get_put,recover_cache_duplication,recover_once
 from models.blip.blip_vqa_visual_encoder import blip_vqa_visual_encoder
 from models.blip.blip_vqa_text_encoder import blip_vqa_text_encoder
 from models.blip.blip_vqa_text_decoder import blip_vqa_text_decoder
 
 
-class blip_vqa_request:
-    def __init__(self,id,image,text,priority,cache):
-        self.id=id
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.start_time_cache = OrderedDict()
+        self.living_cache = OrderedDict()
+        self.end_time_cache = OrderedDict()
 
-        self.image=image
-        self.text=text
-        # todo general content
+    def get(self, key):
+        if key in self.living_cache:
+            if time.perf_counter()<self.end_time_cache[key]:
+                self.cache.move_to_end(key,last=True)
+                self.start_time_cache[key]=time.perf_counter()
+                self.start_time_cache.move_to_end(key,last=True)
+                self.living_cache.move_to_end(key,last=True)
+                self.end_time_cache[key]=self.start_time_cache[key]+self.living_cache[key]
+                self.end_time_cache.move_to_end(key,last=True)
+                return self.living_cache[key]  
+            # in but timeout   
+            del self.start_time_cache[key]
+            del self.living_cache[key]
+            del self.end_time_cache[key]
+        return None
+
+    def put(self, key,living):
+        if living==0:
+            return
+        
+        if key not in self.living_cache and len(self.living_cache) >= self.capacity:
+            self.start_time_cache.popitem(last=False)
+            self.living_cache.popitem(last=False)
+            self.end_time_cache.popitem(last=False)
+
+        self.start_time_cache[key]=time.perf_counter()
+        self.start_time_cache.move_to_end(key,last=True)
+        self.living_cache[key]=living
+        self.living_cache.move_to_end(key,last=True)
+        self.end_time_cache[key]=self.start_time_cache[key]+living
+        self.end_time_cache.move_to_end(key,last=True)
+    
+    def size(self):
+        return len(self.living_cache)
+
+
+class blip_vqa_request:
+    def __init__(self,id,datas,livings,priority):
+        self.id=id
 
         self.start_time=time.perf_counter()
 
         self.priority=priority
         # 0
 
-        self.cache=cache
-        # -1 no cache
+        self.datas=datas
+        # image text
+
+        self.livings=livings
+        # image cache
 
     def set_time(self,time):
         self.start_time=time
@@ -50,7 +91,7 @@ def generate_input(input_queue,start_time_queue,wait_ready):
         with open(json_file) as f:
             dataset = json.load(f)
 
-        request_num_list=[4,4,4,4]
+        # request_num_list=[4,4,4,4]
         request_num_list=[2,4,8,16,32]
 
         request_ids=list(range(request_num_list[0]))
@@ -72,7 +113,7 @@ def generate_input(input_queue,start_time_queue,wait_ready):
                 #image=bytes(os.path.join(dataset_dir, dataset[id]["image"]), "utf-8")
                 image=bytes(os.path.join(dataset_dir, dataset[id]["image"].replace("test2015/", "test2015/re_")), "utf-8")
                 text=bytes(dataset[id]["question"], "utf-8")
-                tmp.append(blip_vqa_request(id,image,text,0,-1))
+                tmp.append(blip_vqa_request(id,[image,text],[0],0))
             request_togethers.append(tmp)
 
         # wait model warm up
@@ -98,13 +139,15 @@ def generate_input(input_queue,start_time_queue,wait_ready):
 def record_output(out_queue,start_time_queue,bs_time,e_time):
     try:
         request_num=start_time_queue.get(block=True)
+        
+        dict_list=[]
 
-        # # start_time
+        # start_time
         start_time_dict={}
         while len(start_time_dict)<request_num:
             start_time_dict.update(start_time_queue.get(block=True))
-
-        dict_list=[]
+        dict_list.append(start_time_dict)
+        
         # bs_time
         bs_time_dict={}
         while len(bs_time_dict)<request_num:
@@ -121,98 +164,171 @@ def record_output(out_queue,start_time_queue,bs_time,e_time):
                 e_time_dict.update(dict(zip(ids,[end_time]*len(ids))))
             dict_list.append(e_time_dict)
         
-        latencies=[]
-        for i in range(request_num):
-            s=f"id={i},start_time={start_time_dict[i]},end_time={dict_list[-1][i]},latency={dict_list[-1][i]-start_time_dict[i]}\n"
-            for d in range(len(dict_list)):
-                s=s+f"{dict_list[d][i]},"
-            s=s+f"\nsche_time={dict_list[0][i]-start_time_dict[i]}"
-            s=s+"\n"
-            for d in range(1,len(dict_list)):
-                s=s+f"{d}_e-{d-1}_e={dict_list[d][i]-dict_list[d-1][i]},"
-            print(s)
-            print()
-            latencies.append(dict_list[-1][i]-start_time_dict[i])
-        
-        print(f"avg latency={sum(latencies)/len(latencies)}")
+        colors = ['red', 'green', 'blue', 'cyan', 'magenta','yellow','black','white','orange','purple','pink','brown',]
+        labels = ['sche','visual','text encoder','text decoder']
+        set_label=False
+        for r in range(request_num):
+            if not set_label:
+                set_label=True
+                for d in range(len(dict_list)-1):
+                    plt.plot([dict_list[d][r],dict_list[d+1][r]],[r]*2,color=colors[d],label=labels[d])
+            else:
+                for d in range(len(dict_list)-1):
+                    plt.plot([dict_list[d][r],dict_list[d+1][r]],[r]*2,color=colors[d])
+        plt.xlabel('time')
+        plt.ylabel('req id')
+        plt.title("time")
+        plt.grid(True)
+        plt.legend()
+        x_min, x_max = min(min(d.values()) for d in dict_list), max(max(d.values()) for d in dict_list)
+        plt.xticks(np.arange(x_min, x_max + 1, 1))
+        plt.savefig('blip_vqa_eevee.png') 
 
         print("record_output finish..................................................")
     except KeyboardInterrupt:
         print("record_output exit..................................................")
 
-def cache_manager(input_queue,cm2c_list,c2cm_list,cm2bs,):
+def cache_manager(input_queue,cm2c_list,c2cm,cm2bs,):
     try:
-        dict_list=[{}]*len(cm2c_list)
+        for i in range(len(cm2c_list)):
+            cm2c_list[i].put(i,block=False)
+            # set cache id
+
+        cache_list=[LRUCache(100) for _ in range(len(cm2c_list))]
+        # cached
+
+        id2keyliving_list=[{} for _ in range(len(cm2c_list))]
+        # ready to cache
+
         while True:
             try:
                 r_b=input_queue.get(block=False)
-                # check cache using dict_list and mark r_b
-                ###########################
+
+                cache_id=[]
+                cache_in=[]
+                for r in r_b:
+                    for i in range(len(r.livings)):
+                        # every req is ready to cache (again)
+                        id2keyliving_list[i][r.id]=[r.datas[i],r.livings[i]]
+
+                        # mark req that is cached
+                        living=cache_list[i].get(r.datas[i])
+                        if living is not None:
+                            r.datas[i]=None
+                        else:
+                            cache_id.append(r.id)
+                            cache_in.append(r.datas[i])
+                
                 cm2bs.put(r_b,block=False)
+                
+                # ask cache to send data to engine
+                cm2c_list[i].put([cache_id,cache_in],block=False)
+
+                # how to delete data in cache engine????????????????????
+                    
+            except queue.Empty:
+                pass
+            try:
+                cache_id,ids=c2cm.get(block=False)
+                for i in ids:
+                    key=id2keyliving_list[cache_id][i][0]
+                    living=id2keyliving_list[cache_id][i][1]
+                    cache_list[cache_id].put(key,living)
+                    del id2keyliving_list[cache_id][i]
             except queue.Empty:
                 pass
     except KeyboardInterrupt:
         print("cache_manager exit..................................................")
 
+def cache_engine(cm2c,c2cm,c2e,e2c):
+    try:
+        cache_id=cm2c.get(block=True)
+        cache={}
+
+        while True:
+            try:
+                ids,in_data,out_data=e2c.get(block=False)
+
+                for i in range(len(ids)):
+                    print("a",in_data[i])
+                    cache[in_data[i]]=out_data[i:i+1]
+
+                c2cm.put([cache_id,ids],block=False)
+                # inform cache manager to add cache info
+            except queue.Empty:
+                pass
+            try:
+                ids,cache_in=cm2c.get(block=False)
+
+                cache_out=[]
+                for i in cache_in:
+                    print("b",i)
+                    cache_out.append(cache[i])
+                
+                c2e.put([ids,cache_out],block=False)
+            except queue.Empty:
+                pass           
+    except KeyboardInterrupt:
+        print(f"cache_{cache_id} exit..................................................")
+    
 def bach_scheduler(cm2bs,bs2e_list,bs_policy,bs_time):
     try:
         # algo need what data struct?
         # append req directly for now
         if bs_policy["kind"]=="fix":
-            data_list0=[]
-            data_ids0=[]
-            data_list1=[]
-            data_ids1=[]
-            data_ids2=[]
-            bs_list=[]
-            bs_list=bs_list+[int(bs) for bs in bs_policy["bses"].split(",")]
+            ids_list=[[] for _ in range(3)]
+            datas_list=[[] for _ in range(3)]
+            livings_list=[[] for _ in range(3)]
+            bs_list=[int(bs) for bs in bs_policy["bses"].split(",")]
             # print(f"bs_list={bs_list}")
             while True:    
                 try:
                     r_b=cm2bs.get(block=False)
                     for r in r_b:
-                        if r.image is not None:
-                            data_list0.append(r.image)
-                            data_ids0.append(r.id)
-                        if r.text is not None:
-                            data_list1.append(r.text)
-                            data_ids1.append(r.id)
-                        data_ids2.append(r.id)
+                        if r.datas[0] is not None:
+                            ids_list[0].append(r.id)
+                            datas_list[0].append(r.datas[0])
+                            livings_list[0].append(r.livings[0])
+                        if r.datas[1] is not None:
+                            ids_list[1].append(r.id)
+                            datas_list[1].append(r.datas[1])
+                        ids_list[2].append(r.id)
                 except queue.Empty:
                     pass
-                if len(data_ids0)!=0:
+                if len(ids_list[0])!=0:
                     # print(f"len(data_list0)={len(data_list0)}")
                     i=0
-                    while i+bs_list[0]<=len(data_ids0):
-                        bs2e_list[0].put([data_ids0[i:i+bs_list[0]],data_list0[i:i+bs_list[0]]],block=False)
-                        bs_time[0].put([data_ids0[i:i+bs_list[0]],time.perf_counter()],block=False)
+                    while i+bs_list[0]<=len(ids_list[0]):
+                        bs2e_list[0].put([ids_list[0][i:i+bs_list[0]],datas_list[0][i:i+bs_list[0]],livings_list[0][i:i+bs_list[0]]],block=False)
+                        bs_time[0].put([ids_list[0][i:i+bs_list[0]],time.perf_counter()],block=False)
                         i+=bs_list[0]
-                    if i<len(data_ids0):
-                        bs2e_list[0].put([data_ids0[i:],data_list0[i:]],block=False)
-                        bs_time[0].put([data_ids0[i:],time.perf_counter()],block=False)
-                    data_list0=[]
-                    data_ids0=[]
-                if len(data_ids1)!=0:
+                    if i<len(ids_list[0]):
+                        bs2e_list[0].put([ids_list[0][i:],datas_list[0][i:],livings_list[0][i:]],block=False)
+                        bs_time[0].put([ids_list[0][i:],time.perf_counter()],block=False)
+                    ids_list[0]=[]
+                    datas_list[0]=[]
+                    livings_list[0]=[]
+                if len(ids_list[1])!=0:
                     i=0
-                    while i+bs_list[1]<=len(data_ids1):
-                        bs2e_list[1].put([data_ids1[i:i+bs_list[1]],data_list1[i:i+bs_list[1]]],block=False)
-                        bs_time[1].put([data_ids1[i:i+bs_list[1]],time.perf_counter()],block=False)
+                    while i+bs_list[1]<=len(ids_list[1]):
+                        bs2e_list[1].put([ids_list[1][i:i+bs_list[1]],datas_list[1][i:i+bs_list[1]]],block=False)
+                        bs_time[1].put([ids_list[1][i:i+bs_list[1]],time.perf_counter()],block=False)
                         i+=bs_list[1]
-                    if i<len(data_ids1):
-                        bs2e_list[1].put([data_ids1[i:],data_list1[i:]],block=False)
-                        bs_time[1].put([data_ids1[i:],time.perf_counter()],block=False)
-                    data_list1=[]
-                    data_ids1=[]
-                if len(data_ids2)!=0:
+                    if i<len(ids_list[1]):
+                        bs2e_list[1].put([ids_list[1][i:],datas_list[1][i:]],block=False)
+                        bs_time[1].put([ids_list[1][i:],time.perf_counter()],block=False)
+                    ids_list[1]=[]
+                    datas_list[1]=[]
+                if len(ids_list[2])!=0:
                     i=0
-                    while i+bs_list[2]<=len(data_ids2):
-                        bs2e_list[2].put([data_ids2[i:i+bs_list[2]],None],block=False)
-                        bs_time[2].put([data_ids2[i:i+bs_list[2]],time.perf_counter()],block=False)
+                    while i+bs_list[2]<=len(ids_list[2]):
+                        bs2e_list[2].put([ids_list[2][i:i+bs_list[2]],None],block=False)
+                        bs_time[2].put([ids_list[2][i:i+bs_list[2]],time.perf_counter()],block=False)
                         i+=bs_list[2]
-                    if i<len(data_ids2):
-                        bs2e_list[2].put([data_ids2[i:],None],block=False)
-                        bs_time[2].put([data_ids2[i:],time.perf_counter()],block=False)
-                    data_ids2=[]
+                    if i<len(ids_list[2]):
+                        bs2e_list[2].put([ids_list[2][i:],None],block=False)
+                        bs_time[2].put([ids_list[2][i:],time.perf_counter()],block=False)
+                    ids_list[2]=[]
 
 
     except KeyboardInterrupt:
@@ -231,21 +347,35 @@ def blip_vqa_visual_encoder_engine(c2e,e2c,pre_queue,bs2e,output_queue,e_time,wa
             wait_ready.put(0,block=False)
 
             while True:    
-                ids,datas=bs2e.get(block=True)
+                try:
+                    ids,in_data,_=bs2e.get(block=True)
 
-                # datas=model.forward_time(datas)
+                    # start_time=time.perf_counter()
+                    out_data=model(in_data)
+                    
+                    # add cache
+                    e2c.put([ids,in_data,out_data],block=False)
 
-                # start_time=time.perf_counter()
-                datas=model(datas)
-                
-                # add cache into ids and datas
-                output_queue.put([ids,datas],block=False) 
 
-                torch.cuda.synchronize()
-                e_time.put([ids,time.perf_counter()],block=False) 
-                
-                # print(f"batch size of blip_vqa_visual_encoder={len(datas)}")
-                
+                    # add cache into ids and datas
+                    # cached_ids,cached_data=c2e.get(block=True)
+                    # how big????
+                    # cache.get()????
+
+                    output_queue.put([ids,out_data],block=False) 
+
+                    torch.cuda.synchronize()
+                    e_time.put([ids,time.perf_counter()],block=False) 
+                    
+                    # print(f"batch size of blip_vqa_visual_encoder={len(datas)}")
+                except queue.Empty:
+                    pass   
+                try:
+                    ids,out_data=c2e.get(block=False)
+                    output_queue.put([ids,out_data],block=False) 
+                    e_time.put([ids,time.perf_counter()],block=False) 
+                except queue.Empty:
+                    pass        
     except KeyboardInterrupt:
         output_queue.close()
         output_queue.cancel_join_thread()
@@ -351,7 +481,6 @@ def blip_vqa_text_decoder_engine(c2e,e2c,pre_queue,bs2e,output_queue,e_time,wait
         print("blip_vqa_text_encoder_engine exit..................................................")
         return 
 
-
 if __name__ == "__main__":
     try:
 
@@ -365,18 +494,17 @@ if __name__ == "__main__":
         input_process=multiprocessing.Process(target=generate_input, args=(input_queue,start_time_queue,wait_ready))
         processes.append(input_process)
 
+        c2cm=multiprocessing.Queue()
         cm2vc=multiprocessing.Queue()
-        vc2cm=multiprocessing.Queue()
         cm2c_list=[cm2vc]
-        c2cm_list=[vc2cm]
         cm2bs=multiprocessing.Queue()
-        cache_manager_process=multiprocessing.Process(target=cache_manager,args=(input_queue,cm2c_list,c2cm_list,cm2bs,))
+        cache_manager_process=multiprocessing.Process(target=cache_manager,args=(input_queue,cm2c_list,c2cm,cm2bs,))
         processes.append(cache_manager_process)
 
         vc2ve=multiprocessing.Queue()
         ve2vc=multiprocessing.Queue()
-        # blip_vqa_visual_encoder_cache=multiprocessing.Process(target=cache_get_put,args=(cm2vc,vc2cm,vc2ve,ve2vc,))
-        # processes.append(blip_vqa_visual_encoder_cache)
+        blip_vqa_visual_encoder_cache_process=multiprocessing.Process(target=cache_engine,args=(cm2vc,c2cm,vc2ve,ve2vc,))
+        processes.append(blip_vqa_visual_encoder_cache_process)
 
         bs2ve=multiprocessing.Queue()
         bs2tee=multiprocessing.Queue()
@@ -398,7 +526,6 @@ if __name__ == "__main__":
         blip_vqa_visual_encoder_engine_process=multiprocessing.Process(target=blip_vqa_visual_encoder_engine,args=(vc2ve,ve2vc,None,bs2ve,v2te,v_time,wait_ready))
         processes.append(blip_vqa_visual_encoder_engine_process)
         
-        
         te2td=multiprocessing.Queue()
         te_time=multiprocessing.Queue()
         blip_vqa_text_encoder_engine_process=multiprocessing.Process(target=blip_vqa_text_encoder_engine,args=(None,None,v2te,bs2tee,te2td,te_time,wait_ready))
@@ -408,7 +535,6 @@ if __name__ == "__main__":
         td_time=multiprocessing.Queue()
         blip_vqa_text_decoder_engine_process=multiprocessing.Process(target=blip_vqa_text_decoder_engine,args=(None,None,te2td,bs2tde,output_queue,td_time,wait_ready))
         processes.append(blip_vqa_text_decoder_engine_process)
-
 
         e_time=[v_time,te_time,td_time]
         output_process=multiprocessing.Process(target=record_output,args=(output_queue,start_time_queue,bs_time,e_time))
